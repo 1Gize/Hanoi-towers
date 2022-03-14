7cd62b6d72abb92bbc4a157b802055e63ac3a011
GrzesiekOlko <s19033@pjwstk.edu.pl>
2022-03-14 18:01:04 +0100

    print tower method added doesnt work properly xd

--- src/HanoiTowers.scala
+++ src/HanoiTowers.scala
@@ -4,5 +4,17 @@ class HanoiTowers {
   var firstStack  = mutable.Stack[Ring]()
   var secondStack = mutable.Stack[Ring]()
   var thirdStack = mutable.Stack[Ring]()
-
+  def createFirstStack(towerSize: Int): Unit ={
+    var x = towerSize
+    while(x > 0){
+      firstStack.addOne(new Ring(x))
+      x = x - 1
+    }
+  }
+  def printTower(stack: mutable.Stack[Ring]): Unit={
+    for(s <- stack) {
+      println(stack.top.name)
+      stack.pop()
+    }
+  }
 }
