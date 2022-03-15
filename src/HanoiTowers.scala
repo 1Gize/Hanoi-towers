64939edceaa10a97ea59214e143a97ea7425b0ca
GrzesiekOlko <s19033@pjwstk.edu.pl>
2022-03-15 13:02:48 +0100

    added method to move rings from one tower to other added method to check if move is valid

--- src/HanoiTowers.scala
+++ src/HanoiTowers.scala
@@ -5,16 +5,35 @@ class HanoiTowers {
   var secondStack = mutable.Stack[Ring]()
   var thirdStack = mutable.Stack[Ring]()
   def createFirstStack(towerSize: Int): Unit ={
-    var x = towerSize
-    while(x > 0){
+    var x = 1
+    while(x <= 0){
       firstStack.addOne(new Ring(x))
-      x = x - 1
+      x = x +  1
     }
   }
   def printTower(stack: mutable.Stack[Ring]): Unit={
     for(s <- stack) {
-      println(stack.top.name)
-      stack.pop()
+      println(s.name)
+    }
+  }
+  def moveRing(stack1: mutable.Stack[Ring], stack2: mutable.Stack[Ring]): Unit={
+    val ring1 = stack1.top
+    if(stack2.isEmpty){
+      stack2.push(ring1)
+      stack1.pop()
+    }else if(checkIfMoveIsValid(ring1,stack2.top)){
+      stack2.push(ring1)
+      stack1.pop()
+    }else{
+      println("Please chose different move, this one is invalid")
+    }
+  }
+  def checkIfMoveIsValid(ring1: Ring, ring2: Ring): Boolean={
+    if(ring1.width < ring2.width) {
+      true
+    }else{
+      //println("Invalid move!")
+      false
     }
   }
 }
